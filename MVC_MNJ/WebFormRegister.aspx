ed6931acb35a66a5f073cc7855e416aebfa71a74
cwjh63 <aliff.rahim@motorolasolutions.com>
2020-03-12 20:24:15 +0800

    Init

---
+++ MVC_MNJ/WebFormRegister.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebFormRegister.aspx.cs" Inherits="MVC_MNJ.WebFormRegister" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form_Register" runat="server">
+        <div>
+            <div>
+                <asp:Label ID="Label_Name" runat="server" Text="Name : - "></asp:Label>
+                <asp:TextBox ID="TextBox_Name" runat="server"></asp:TextBox>
+                <br/>
+                <asp:Label ID="Label_Age" runat="server" Text="Age : - "></asp:Label>
+                <asp:TextBox ID="TextBox_Age" runat="server"></asp:TextBox>
+                <br/>
+                <asp:Label ID="Label_Address" runat="server" Text="Address : - "></asp:Label>
+                <asp:TextBox ID="TextBox_Address" runat="server"></asp:TextBox>
+                <br/>
+                <asp:Label ID="Label_Phone" runat="server" Text="Phone No : - "></asp:Label>
+                <asp:TextBox ID="TextBox_Phone" runat="server"></asp:TextBox>
+                <br/>
+                <br/>
+                <div>
+                    <asp:Button ID="Button_Add" runat="server" Text="Add" OnClick="Button_Add_Click" />
+                    <asp:Button ID="Button_Update" runat="server" Text="Update" OnClick="Button_Update_Click"/>
+                    <asp:Button ID="Button_Delete" runat="server" Text="Delete" OnClick="Button_Delete_Click"/>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MVC_MNJ/WebFormRegister.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace MVC_MNJ
+{
+    public partial class WebFormRegister : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_Add_Click(object sender, EventArgs e)
+        {
+            SQL_command("INSERT INTO Table_Register (Name, Age, Address, Phone) "
+                + "VALUES(" + "'" + TextBox_Name.Text + "' , '" + TextBox_Age.Text + "' , '" + TextBox_Address.Text + "' , '" + TextBox_Phone.Text + "')");
+        }
+
+        protected void Button_Update_Click(object sender, EventArgs e)
+        {
+            SQL_command("UPDATE Table_Register SET Name = '" + TextBox_Name.Text + "', Age = '" + TextBox_Age.Text + "', Address = '" + TextBox_Address.Text + "', Phone = '" + TextBox_Phone.Text + "' WHERE Name = '" + TextBox_Name.Text + "'");
+        }
+
+        protected void Button_Delete_Click(object sender, EventArgs e)
+        {
+            SQL_command("DELETE FROM Table_Register WHERE Name = '" + TextBox_Name.Text + "'");
+        }
+
+        private void SQL_command(string Command)
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = @"Data Source=CWJH63-01\SQLEXPRESS;Initial Catalog=LoginDBMNJ;Integrated Security=True";
+            SqlCommand command = new SqlCommand(Command, conn);
+            conn.Open();
+            command.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
